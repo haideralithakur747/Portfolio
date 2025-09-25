98bba7791f5d2b83dffd96bf7a455cdb0fe9abe2
Haider ALi <haideralithakur747@gmail.com>
2025-09-25 15:28:02 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Home from "./Home";
 import SkillsCarousel from "./skills";
 import EducationExperience from "./Edu&exp";
 import Projects from "./Projects";   
-import Contact from "./Contact";       
-
+import Contact from "./Contact";
+import Footer from "./Footer";
 import "./App.css"
 export default function App() {
-    return(
+    return (
         <>
         <Header />
         <Home />
@@ -17,6 +17,8 @@ export default function App() {
         <EducationExperience />
         <Projects />
         <Contact />
+        <Footer />
+     
         </>
     )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return(
+          <p style={{ textAlign: "center", padding: "1rem", backgroundColor: "#111", color: "white",margin:"0px" }}>
+            © {new Date().getFullYear()} Haider Ali | All Rights Reserved</p>
+    )
+}
